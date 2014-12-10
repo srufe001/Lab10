bd47ceee95dcae8139e07934e97b23a513d5a4ad
srufer <srufe001@ucr.edu>
2014-12-09 18:32:47 -0800

    added but did not test Music_collection.h and .cpp

--- Music_collection.cpp
+++ Music_collection.cpp
@@ -1,5 +1,5 @@
 //  =============== BEGIN ASSESSMENT HEADER ================
-/// @file Lab10/
+/// @file Lab10/Music_collection.cpp
 /// @brief Lab 10 for CS 12 Fall 2014
 ///
 /// @author Samuel Rufer [srufe001@ucr.edu]
@@ -16,3 +16,99 @@
 /// I hereby certify that the code in this file
 /// is ENTIRELY my own original work.
 //  ================== END ASSESSMENT HEADER ===============
+#include "Music_collection.h"
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+Music_collection::Music_collection()
+: max(100), number(0), collection(new Tune [max])
+{ }
+
+Music_collection::Music_collection(int n)
+: max(n), number(0), collection(new Tune [max])
+{ }
+
+Music_collection::~Music_collection()
+{
+   delete[] collection;
+}
+
+Music_collection::Music_collection(const Music_collection & m)
+: max(m.max), number(m.number), collection(new Tune [max])
+{
+   //copy each tune from m into this
+   for (int i = 0; i < number; ++i)
+   {
+      collection[i] = m.collection[i];
+   }
+}
+
+Music_collection & Music_collection::operator=(const Music_collection & m)
+{
+   //check for self-assignment
+   if (this != &m)
+   {
+      //shallow-copy max and number
+      max = m.max;
+      number = m.number;
+      //delete and recreate current collection
+      delete[] collection;
+      collection = new Tune [max];
+      //deep-copy collection
+      for (int i = 0; i < number; ++i)
+      {
+         collection[i] = m.collection[i];
+      }
+   }
+   //return itself
+   return *this;
+}
+
+bool Music_collection::add_tune(const Tune & t)
+{
+   //check if collection is full
+   if (number == max) return false;
+
+   //add the tune and increment number of tunes
+   collection[number++] = t;
+   return true;
+}
+
+bool Music_collection::set_tune(int index, const Tune & t)
+{
+   //check if index already contains a tune
+   if (index >= number) return false; 
+
+   //insert the new tune
+   collection[index] = t;
+
+   //return true since a tune was replaced
+   return true;
+}
+
+ostream & operator<<(ostream & out, const Music_collection & m)
+{
+   //add all tunes to the stream, with newlines after each tune
+   for (int i = 0; i < m.number; ++i)
+   {
+      out << m.collection[i].get_title() << "\n";
+   }
+   return out;
+}
+
+//Tune methods
+
+Tune::Tune()
+: title()
+{ }
+
+Tune::Tune(const string & n)
+: title(n)
+{ }
+
+const string & Tune::get_title() const
+{
+   return title;
+}
--- Music_collection.h
+++ Music_collection.h
@@ -1,5 +1,5 @@
 //  =============== BEGIN ASSESSMENT HEADER ================
-/// @file Lab10/
+/// @file Lab10/Music_collection.h
 /// @brief Lab 10 for CS 12 Fall 2014
 ///
 /// @author Samuel Rufer [srufe001@ucr.edu]
@@ -16,3 +16,59 @@
 /// I hereby certify that the code in this file
 /// is ENTIRELY my own original work.
 //  ================== END ASSESSMENT HEADER ===============
+#ifndef __MUSIC_COLLECTION_H__
+#define __MUSIC_COLLECTION_H__
+
+#include<string>
+
+using namespace std;
+
+//Tune class
+
+class Tune
+{
+  private:
+   string title;
+  public:
+   Tune();
+   Tune(const string & n);
+   const string & get_title() const;
+};
+
+//Music_collection class
+
+class Music_collection
+{
+  private:
+   int number;
+   int max;
+   Tune * collection;
+  public:
+   //Constructors
+   //sets max to 100
+   Music_collection();
+   //sets max to n
+   Music_collection(int n);
+
+   //Destructor
+   ~Music_collection();
+
+   //Copy constructor
+   Music_collection(const Music_collection &m);
+
+   //copy assignment operator
+   Music_collection & operator=(const Music_collection &m);
+
+   //returns true if add was successful, false otherwise
+   bool add_tune(const Tune &t);
+
+   //sets Tune at position index in collection to t, returns true if
+   //index < number
+   bool set_tune(int index, const Tune &t);
+
+   //overloaded stream insertion operator
+   //outputs the title fo each Tune in collection on a seperate line
+   friend ostream & operator<<(ostream &out, const Music_collection &m);
+};
+
+#endif
