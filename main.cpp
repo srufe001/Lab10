1f3e10ef0ca06af73aea4fbf28ce275117e490c4
srufe001 <srufe001@ucr.edu>
2014-12-10 01:29:27 +0000

    added CS12 headers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file Lab10/
+/// @brief Lab 10 for CS 12 Fall 2014
+///
+/// @author Samuel Rufer [srufe001@ucr.edu]
+/// @date December 9, 2013
+///
+/// @par Enrollment Notes 
+///     Lecture Section: 001
+/// @par
+///     Lab Section: 022
+/// @par
+///     TA: Ali
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  ================== END ASSESSMENT HEADER ===============
+
 #include<iostream>
 
 using namespace std;
 
-int main()
+int main
 {
 
    return 0;
